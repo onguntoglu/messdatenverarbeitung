2cce15cbc4a6420993bfb4ba6f5e8bd84ae8bd23
Ongun Türkcüoglu <ongun.turkcuoglu@campus.tu-berlin.de>
2018-05-08 21:22:47 +0200

    2.0

--- pr1/vorbereitungsaufgaben/aufgabe2.c
+++ pr1/vorbereitungsaufgaben/aufgabe2.c
@@ -8,20 +8,20 @@
 
 
 void controlLEDs(uint16_t value){
-	if(value <= 0 && value <= 127){
-		
+	if(value >= 0 && value <= 127){
+		PORTC |= (0 << PC1) | (0 << PC2) | (0 << PC3);  // Turn off LEDs
 	}
 
-	else if(value <= 128 && value <= 511){
-
+	else if(value >= 128 && value <= 511){
+		PORTC |= (1 << PC1) | (0 << PC2) | (0 << PC3);  // Turn on red
 	}
 
-	else if(value <= 128 && value <= 511){
-
+	else if(value >= 512 && value <= 768){
+		PORTC |= (1 << PC1) | (1 << PC2) | (0 << PC3);  // Turn on red and green
 	}
 
-	else if(value <= 128 && value <= 511){
-
+	else if(value >= 769 && value <= 1023){
+		PORTC |= (1 << PC1) | (1 << PC2) | (1 << PC3); // Turn on red, green and orange
 	}
 
 }
@@ -49,6 +49,10 @@ int main(void){
 	init(); // run func init()
 	sei();	// enable global interrupts
 	ADCSRA |= (1 << ADSC); // start ADC conversation
+
+	DDRC |= (1<<PC1);
+	PORTC |= (1<<PC1);
+
 		while(1){
 		}
 }
