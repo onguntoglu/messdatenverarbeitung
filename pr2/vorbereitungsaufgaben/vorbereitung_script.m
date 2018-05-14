c4060714cbc3074bd6c17bcdba9664f19c965373
Ongun Türkcüoglu <ongun.turkcuoglu@campus.tu-berlin.de>
2018-05-14 23:33:51 +0200

    1.1

--- pr2/vorbereitungsaufgaben/vorbereitung_script.m
+++ pr2/vorbereitungsaufgaben/vorbereitung_script.m
@@ -1,6 +1,7 @@
 %% Aufgabe 2 - Butterworth 2. Ordnung
 clear
-fg = 3100;
-[b,a] = butter(2, 2*pi*fg); % 2. Ordnung Butterworth, Grenzfreq 3.1kHz
+fc = 3100;  % cutoff frequency (Grenzfrequenz)
+
+[b,a] = butter(2, 1); % 2. Ordnung Butterworth
 sys_butter2 = tf(b,a);
 bode(sys_butter2)
