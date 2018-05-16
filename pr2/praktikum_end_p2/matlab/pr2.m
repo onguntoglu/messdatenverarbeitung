ad8343d59bb8cf5c62ab3529fb0d11436cfb7aac
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-05-16 18:00:15 +0200

    final pr2 added
    pr2 finalized

--- pr2/praktikum_end_p2/matlab/pr2.m
+++ pr2/praktikum_end_p2/matlab/pr2.m
@@ -2,6 +2,7 @@
 aduwerte_a1 = ucAnalogRead('COM7',5000,1000,'NONE',0,'OFF');
 plot(aduwerte_a1)
 %% Aufgabe 2 - Dreiecksignal ADU
+clear
 clf
 aduwerte_a2 = ucAnalogRead('COM7',5000,1000,'NONE',0,'OFF');
 plot(aduwerte_a2)
@@ -13,8 +14,23 @@ while(aduwerte_a2(index_min_aduwerte2 + i) < aduwerte_a2(index_min_aduwerte2 + i
 end
 clf
 flanke = aduwerte_a2(index_min_aduwerte2: index_min_aduwerte2 + i);
-plot(flanke)
-save('aduwerte_a2.mat', 'aduwerte_a2');
-save('flanke.mat', 'flanke');
+% plot(flanke)
+% save('aduwerte_a2.mat', 'aduwerte_a2');
+% save('flanke.mat', 'flanke');
+x = linspace(-7,7,length(flanke));
+plot(x,flanke)
+grid on
+hold on
+p = polyfit(x,flanke',1)
+steigung_adu = p(1);
+offset_adu = p(2);
+steigung_theo = 1023/14;
+p2 = steigung_theo*x;
+plot(x,p2)
+title('ADU-Codierung')
+xlabel('Spannung in Volt')
+ylabel('Binary-Codes')
+legend('ADU-real','ADU-ideal')
+
 
 
