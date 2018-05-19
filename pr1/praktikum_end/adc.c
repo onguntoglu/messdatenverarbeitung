b1749739a60a47187ce530997efa86fbec0d5f79
Ongun Türkcüoglu <ongun.turkcuoglu@campus.tu-berlin.de>
2018-05-19 20:29:36 +0200

    working version of adc.c uploaded

--- pr1/praktikum_end/adc.c
+++ pr1/praktikum_end/adc.c
@@ -38,22 +38,19 @@ ISR(ADC_vect) {
 	uint16_t valADC = ADC;		// alternatively read result from ADC
 	uint16_t valADC_offset = valADC - 512;
 	filterWrite2Buf(valADC_offset);
-	
-	ADCSRA |= (1 << ADSC);	
 
 	if(counter == sampleCountGlobal){
-		ADCSRA |= (0 << ADIE); // ADC-Interrupt turn off
-		TCCR1B |= (0 << CS10) | (0 << CS11) | (0 << CS12); // turn off Timer
+		ADCSRA &= ~(1 << ADIE); // ADC-Interrupt turn off
+		TCCR1B &= ~((1 << CS10) | (1 << CS11) | (1 << CS12)); // turn off Timer
 		counter = 0;
 		sampleCountGlobal = 0;	// both set to 0
-		cli(); // deactive global interrupts
 	}
 
 }
 
 /*** FUNCTIONS ***/
 void adcInit() {
-
+	cli();
 	ADMUX |= (1 << REFS1) | (1 << REFS0); // 2.56V reference voltage for ADC
 	ADCSRB |= (0 << MUX0) | (0 << MUX1) | (0 << MUX2) | (0 << MUX3) | (0 << MUX4); // select ADC0
 	ADCSRA |= (1 << ADATE); // Auto-trigger mdoe enabled
@@ -62,14 +59,16 @@ void adcInit() {
 	ADCSRA |= (1 << ADIE); // enable ADC
 	ADCSRA |= (1 << ADEN); // enable ADC-Interrupt
 	//ADCSRA |= (1 << ADSC); // start ADC-conversion
+	sei();
 
 }
 
 void adcStart(uint16_t sampleRateCode, uint32_t sampleCount, trigger_t triggerMode, int16_t triggerLevel) {
+	cli();
 	uint32_t sampleCountGlobal = sampleCount;	// global Var for sampleCount
 	OCR1A = sampleRateCode;	// set Compare-Match to var sampleRateCode
 	TCNT1 = 0;				// start Timer at 0
-	TIMSK1 = (1 << OCIE1A) | (1 << OCIE2B); // enable Timer-Compare-Match Interrupt
+	TIMSK1 = (1 << OCIE1A) | (1 << OCIE1B); // enable Timer-Compare-Match Interrupt
 	TCCR1A = 0; // Output-Compare disable
 	TCCR1B |= (1 << CS10); // Timer prescaler set to CPU-clock
 	TCCR1B |= (1 << WGM12); // CTC-Modus set
