bc9875484e8cd5a725a241425b211a65516510ac
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-05-30 17:09:15 +0200

    pr3 uploaded

---
+++ pr3/praktikum_end_p3/doc/html/modules.js
@@ -0,0 +1,8 @@
+var modules =
+[
+    [ "Analog to Digital Conversion", "group__adc.html", null ],
+    [ "Command Interpreter", "group__command.html", null ],
+    [ "FIFO-Buffer", "group__fifo.html", null ],
+    [ "Digital Filtering", "group__filter.html", null ],
+    [ "Serial Port Communication", "group__serial.html", null ]
+];
---
+++ pr3/praktikum_end_p3/matlab/notizen.m
@@ -0,0 +1,6 @@
+%aduwerte12ms -> Stromeffektivwert 0.275332A
+%aduwerte62ms -> Stromeffektivwert 0.185541A
+%aduwerte34ms -> Stromeffektivwert 0.253212A
+
+%SpannungToStrom-Faktor -> Vi = 10V/0.9A
+%aduwerte Code2Volt -> SpannungToStrom-Faktor -> MATLAB-Funktion benutzen
