5f40c257818a8f2e794827b1cecc813c61bb57a8
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-05-31 14:22:52 +0200

    EffektivwertFourier changed

--- pr3/vorbereitungsaufgaben/EffektivwertFourier.m
+++ pr3/vorbereitungsaufgaben/EffektivwertFourier.m
@@ -1,7 +1,7 @@
 function RMSIf = EffektivwertFourier(xt)
 
 DFTxt = fft(xt);
-RMSIf = (1/length(DFTxt)) * sum(power(abs(DFTxt),2));
+RMSIf = (1/length(DFTxt)) *sqrt(sum(power(abs(DFTxt),2)));
 
 end
 
