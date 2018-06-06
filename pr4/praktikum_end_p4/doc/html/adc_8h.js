ae56ffd5a32984bb64272c14dd38c2e2bb05e641
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-06-06 17:46:29 +0200

    pr4 completed

---
+++ pr4/praktikum_end_p4/doc/html/adc_8h.js
@@ -0,0 +1,9 @@
+var adc_8h =
+[
+    [ "ADC_OFFSET", "group__adc.html#ga827907ee6183e98bb8a50727540d4bb1", null ],
+    [ "trigger_t", "group__adc.html#ga7525d66767406deb7a8b51f927aae2b7", null ],
+    [ "trigger", "group__adc.html#gae194cb817eae4085f8023885100c68dd", null ],
+    [ "adcInit", "group__adc.html#ga27c5e947cca4b8e98323253543557b23", null ],
+    [ "adcIsRunning", "group__adc.html#ga3e7a7e2c420118e7f3cf5d14ac390b40", null ],
+    [ "adcStart", "group__adc.html#gabc524700e7bcb90a38cc9146c5e29aba", null ]
+];
---
+++ pr4/praktikum_end_p4/doc/html/files.js
@@ -0,0 +1,16 @@
+var files =
+[
+    [ "adc.c", "adc_8c.html", "adc_8c" ],
+    [ "adc.h", "adc_8h.html", "adc_8h" ],
+    [ "command.c", "command_8c.html", "command_8c" ],
+    [ "command.h", "command_8h.html", "command_8h" ],
+    [ "fifo.c", "fifo_8c.html", "fifo_8c" ],
+    [ "fifo.h", "fifo_8h.html", "fifo_8h" ],
+    [ "filter.c", "filter_8c.html", "filter_8c" ],
+    [ "filter.h", "filter_8h.html", "filter_8h" ],
+    [ "filter_coeff.h", "filter__coeff_8h.html", "filter__coeff_8h" ],
+    [ "main.h", "main_8h.html", "main_8h" ],
+    [ "MDV_PR.c", "_m_d_v___p_r_8c.html", "_m_d_v___p_r_8c" ],
+    [ "serial.c", "serial_8c.html", "serial_8c" ],
+    [ "serial.h", "serial_8h.html", "serial_8h" ]
+];
