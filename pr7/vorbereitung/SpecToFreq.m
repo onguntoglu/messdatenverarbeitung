86f2677400014f6d5a75cd1530ad864c3871dd9f
onguntoglu <30687798+onguntoglu@users.noreply.github.com>
2018-07-04 16:17:47 +0200

    pr7 vorbereitung

--- pr7/vorbereitung/SpecToFreq.m
+++ pr7/vorbereitung/SpecToFreq.m
@@ -1,7 +1,15 @@
-function [ output_args ] = SpecToFreq( input_args )
-%SPECTOFREQ Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function output = SpecToFreq(signal, window, fs)
+    [s, f, t] = spectogram(signal, window, fs);
+    
+    dummy_vec = zeros(1,length(s(:,1)));
+    for i=1:length(s(1,:)) % first column contain frequencies
+        val = s(:,i);      % i-tes Element
+        abs_val = abs(val); % abs of complex number
+        [~, id] = max(abs_val);
+        frequenz = f(id);
+        dummy_vec(i) = frequenz;
+    end
+    
+    output = dummy_vec;
 end
 
